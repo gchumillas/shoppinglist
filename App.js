f49349d8ede90324e47e38479cba5d130fee3cbc
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-11-09 18:28:04 +0100

    update

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, FlatList, Text } from 'react-native';
+
+const articles = [
+  { text: 'Pimiento rojo', id: '1' },
+  { text: 'Guindillas', id: '2' },
+  { text: 'Patatas pequeñas', id: '3' },
+  { text: 'Comino en grano', id: '4' },
+  { text: 'Vinagre', id: '5' },
+  { text: 'Ajos', id: '6' },
+  { text: 'Pimentón dulce', id: '7' },
+  { text: 'Sal', id: '8' }
+]
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => <Text key={item.id}>{item.text}</Text>}
+      />
       <StatusBar style="auto" />
     </View>
   );
