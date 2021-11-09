aa0f50b3db8d02c33b07f4dcc6aa3fbf51368ca1
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-11-09 18:42:07 +0100

    lint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,31 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "react-native/react-native": true
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'react-native/react-native': true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended'
     ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
         },
-        "ecmaVersion": 13,
-        "sourceType": "module"
+        'ecmaVersion': 13,
+        'sourceType': 'module'
     },
-    "plugins": [
-        "react",
-        "react-native"
+    'plugins': [
+        'react',
+        'react-native'
     ],
-    "rules": {
-        "semi": ["error", "never"],
-        "quotes": ["error", "single"],
-        "react-native/no-unused-styles": 2,
-        "react-native/split-platform-components": 2,
-        "react-native/no-inline-styles": 2,
-        "react-native/no-raw-text": 2,
-        "react-native/no-single-element-style-arrays": 2
+    'rules': {
+        'semi': ['error', 'never'],
+        'quotes': ['error', 'single'],
+        'react-native/no-unused-styles': 2,
+        'react-native/split-platform-components': 2,
+        'react-native/no-inline-styles': 2,
+        'react-native/no-raw-text': 2,
+        'react-native/no-single-element-style-arrays': 2
     }
-};
+}
