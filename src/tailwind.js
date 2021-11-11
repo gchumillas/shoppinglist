a6827a4aa92dd2071f73f67791fbb5b028af7acd
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-11-11 18:52:06 +0100

    rename file

--- src/tailwind.js
+++ src/tailwind.js
@@ -1,5 +1,5 @@
 import { create } from 'tailwind-rn'
-import styles from '~/styles.json'
+import styles from '~/tailwind.styles.json'
 
 const { tailwind, getColor } = create(styles)
 export { tailwind, getColor }
