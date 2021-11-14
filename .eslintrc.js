706a33d2ae741391ce48b16948c92d8d7573d4d4
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-11-14 20:17:01 +0100

    refactor

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,10 +23,6 @@ module.exports = {
         "react"
     ],
     "rules": {
-        "multiline-ternary":  "off",
-        "camelcase": "off", // some packages exposes non-camelcase entities
-        "semi": ["error", "never"],
-        "quotes": ["error", "single"],
         "react/prop-types": "off",
         "react/jsx-indent": [2, 2],
         "react/jsx-indent-props": [2, 2]
--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { StyleSheet, View, FlatList, StatusBar, ActivityIndicator } from 'react-native'
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar'
 import { useFonts } from 'expo-font'
-import { IndieFlower_400Regular } from '@expo-google-fonts/indie-flower'
+import { IndieFlower_400Regular as indieFlower400Regular } from '@expo-google-fonts/indie-flower'
 import { Text } from '~/src/elements'
 import { tailwind, getColor } from '~/src/tailwind'
 
@@ -28,12 +28,14 @@ const App = () => {
 }
 
 const AppInit = () => {
-  const [fontsLoaded] = useFonts({ IndieFlower_400Regular })
+  const [fontsLoaded] = useFonts({ indieFlower400Regular })
 
-  return fontsLoaded ? <App /> : <View style={styles.container}>
-    <ActivityIndicator size="large" color={getColor('gray-600')} />
-    <ExpoStatusBar style="auto" />
-  </View>
+  return fontsLoaded
+    ? <App />
+    : <View style={styles.container}>
+      <ActivityIndicator size="large" color={getColor('gray-600')} />
+      <ExpoStatusBar style="auto" />
+    </View>
 }
 
 const styles = StyleSheet.create({
