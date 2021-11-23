c8f9e6b91399cd04bcef576f41c4654440a336b0
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-11-23 18:38:05 +0100

    routing: refactor

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+import React from 'react'
 import { registerRootComponent } from 'expo'
+import { NativeRouter, Routes, Route } from 'react-router-native'
+import Home from '~/src/pages/Home'
 import App from './src/App'
 
-registerRootComponent(App)
+const Root = () => {
+  return <NativeRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  </NativeRouter>
+}
+
+registerRootComponent(Root)
--- src/App.js
+++ src/App.js
@@ -3,20 +3,16 @@ import { StyleSheet, View, ActivityIndicator, ImageBackground } from 'react-nati
 import { StatusBar } from 'expo-status-bar'
 import { useFonts } from 'expo-font'
 import { PatrickHand_400Regular as patrickHand400Regular } from '@expo-google-fonts/patrick-hand'
-import { NativeRouter, Routes, Route } from 'react-router-native'
-import Home from '~/src/pages/Home'
+import { Outlet } from 'react-router-native'
+// import Home from '~/src/pages/Home'
 import { tw, getColor } from '~/src/libs/tailwind'
 
 const App = () => {
   // TODO: (?) replace ImageBackground by a canvas
-  return <NativeRouter>
-    <ImageBackground source={require('~/assets/bg.png')} style={styles.container}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <StatusBar style="auto" />
-    </ImageBackground>
-  </NativeRouter>
+  return <ImageBackground source={require('~/assets/bg.png')} style={styles.container}>
+    <Outlet />
+    <StatusBar style="auto" />
+  </ImageBackground>
 }
 
 const AppInit = () => {
