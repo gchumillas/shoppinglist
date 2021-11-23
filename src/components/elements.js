0672b98d3fb5dcf3aa6db2fbcd26246cca7d184c
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-11-23 21:29:12 +0100

    refactor

--- src/components/elements.js
+++ src/components/elements.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import rn from 'react-native'
+import { Text } from 'react-native'
 import { tw } from '~/src/libs/tailwind'
 
 const AppText = ({ style, ...props }) => {
   const defaultStyle = { ...tw('text-base text-gray-800'), fontFamily: 'patrickHand400Regular' }
 
-  return <rn.Text {...props} style={{ ...defaultStyle, ...style }} />
+  return <Text {...props} style={{ ...defaultStyle, ...style }} />
 }
 
 export { AppText }
