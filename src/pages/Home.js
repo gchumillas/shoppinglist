3a951a1080cfc0bc0c474788eb4efdc891d8871d
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-11-24 20:53:43 +0100

    add options button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import { StyleSheet, View, FlatList, Pressable, StatusBar } from 'react-native'
 import { Link, Outlet, useLocation } from 'react-router-native'
 import NewIcon from '~/assets/icons/new.svg'
 import DeleteIcon from '~/assets/icons/delete.svg'
+import OptionsIcon from '~/assets/icons/options.svg'
 import { tw } from '~/src/libs/tailwind'
 import { AppText } from '~/src/components/elements'
 import { getArticles } from '~/src/providers/articles'
@@ -22,7 +23,12 @@ const Component = () => {
     <View style={styles.list}>
       <FlatList
         data={articles}
-        renderItem={({ item }) => <AppText key={item.id} style={styles.item}>{item.text}</AppText>}
+        renderItem={({ item }) => <View style={styles.itemWrapper}>
+          <AppText key={item.id} style={styles.itemText}>{item.text}</AppText>
+          <Pressable>
+            <OptionsIcon />
+          </Pressable>
+        </View>}
         keyExtractor={item => item.id} />
     </View>
     <View style={styles.footer}>
@@ -42,7 +48,8 @@ const styles = StyleSheet.create({
     ...tw('flex-shrink flex-grow py-3 px-4'),
     marginTop: StatusBar.currentHeight || 0
   },
-  item: tw('text-2xl p-2 m-1'),
+  itemWrapper: tw('flex flex-row items-center justify-between'),
+  itemText: tw('text-2xl p-2 m-1'),
   footer: tw('flex flex-row justify-evenly items-center py-3')
 })
 
