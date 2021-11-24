c54328071eda150e5da0bfba46887ca83ccabced
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-11-24 18:07:01 +0100

    AppTextInput: add title prop

--- src/components/elements/AppTextInput.js
+++ src/components/elements/AppTextInput.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { TextInput, StyleSheet } from 'react-native'
+import { TextInput, StyleSheet, View, Text } from 'react-native'
 import { tw } from '~/src/libs/tailwind'
 
-const Component = ({ ...props }) => {
-  return <TextInput {...props} style={styles.input} />
+const Component = ({ title = '', ...props }) => {
+  return <View style={styles.wrapper}>
+    {!!title && <Text style={styles.title}>{title}</Text>}
+    <TextInput {...props} style={styles.input} />
+  </View>
 }
 
 const styles = StyleSheet.create({
+  wrapper: tw('mb-4'),
+  title: tw('text-xs'),
+  // TODO: add color palette (for example text-primary, etc....)
   input: tw('border-b-2 text-gray-800')
 })
 
