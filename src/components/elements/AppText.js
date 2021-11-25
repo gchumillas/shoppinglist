14f5e3ffc4676ef7a8636b3abc2613a3330d7ff9
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-11-25 21:22:51 +0100

    add inline to-do

--- src/components/elements/AppText.js
+++ src/components/elements/AppText.js
@@ -3,6 +3,7 @@ import { Text } from 'react-native'
 import { tw } from '~/src/libs/tailwind'
 
 const Component = ({ style, ...props }) => {
+  // TODO: replace patrickHand400Regular by Roboto
   const defaultStyle = { ...tw('text-base text-primary'), fontFamily: 'patrickHand400Regular' }
 
   return <Text {...props} style={{ ...defaultStyle, ...style }} />
