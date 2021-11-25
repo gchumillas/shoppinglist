cd91d0c097a69d2dd629fd36b3768747a8052d1f
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-11-25 19:56:23 +0100

    tailwind, config: add primary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'primary': colors.gray[800],
+        'primary-light': colors.white
+      },
+    }
   },
   variants: {
     extend: {}
