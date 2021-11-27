69ad265256ac749197da3e7955900d6df0d791bf
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-11-27 17:26:27 +0100

    pages: add DeleteAllArticles

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { registerRootComponent } from 'expo'
 import { NativeRouter, Routes, Route } from 'react-router-native'
 import NewArticle from '~/src/pages/NewArticle'
 import EditArticle from '~/src/pages/EditArticle'
+import DeleteAllArticles from './src/pages/DeleteAllArticles'
 import App from './src/App'
 
 const Root = () => {
@@ -11,6 +12,7 @@ const Root = () => {
       <Route path="/" element={<App />}>
         <Route path="/new-article" element={<NewArticle />} />
         <Route path="/edit-article/:id" element={<EditArticle />} />
+        <Route path="/delete-all-articles" element={<DeleteAllArticles />} />
       </Route>
     </Routes>
   </NativeRouter>
---
+++ src/pages/DeleteAllArticles.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+import { useNavigate } from 'react-router-native'
+import { tw } from '~/src/libs/tailwind'
+import { AppButton, AppText } from '~/src/components/elements'
+import ModalBox from '~/src/components/ModalBox'
+import { deleteAllArticles } from '~/src/providers/articles'
+import { context } from './Home'
+
+const Component = () => {
+  const { reload } = React.useContext(context)
+  const navigate = useNavigate()
+
+  // TODO: (all) enforce _ vs. ()
+  const doDeleteAll = async _ => {
+    await deleteAllArticles()
+    reload()
+    navigate('/')
+  }
+
+  return <ModalBox visible>
+    <View>
+      <AppText style={tw('text-center pb-6')}>Delete all articles?</AppText>
+    </View>
+    <View style={styles.footer}>
+      <AppButton title="No" onPress={() => navigate('/')} />
+      <AppButton title="Yes" primary onPress={doDeleteAll} />
+    </View>
+  </ModalBox>
+}
+
+const styles = StyleSheet.create({
+  footer: tw('flex flex-row items-center justify-around')
+})
+
+export default Component
