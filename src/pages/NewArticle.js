6be3bf413b0ff493fe632b97a41d8ed19170d556
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-11-27 17:22:46 +0100

    codefixes

--- src/pages/NewArticle.js
+++ src/pages/NewArticle.js
@@ -20,7 +20,7 @@ const Component = () => {
 
   return <ModalBox visible>
     <View>
-      <AppTextInput autoFocus value={text} onChangeText={setText} style={styles.input} />
+      <AppTextInput autoFocus value={text} onChangeText={setText} />
     </View>
     <View style={styles.footer}>
       <AppButton title="Close" onPress={() => navigate('/')} />
@@ -30,9 +30,6 @@ const Component = () => {
 }
 
 const styles = StyleSheet.create({
-  container: tw('flex h-full justify-center items-center'),
-  box: tw('bg-white p-5 max-w-xs w-full border rounded-lg'),
-  input: tw('border'),
   footer: tw('flex flex-row items-center justify-around')
 })
 
