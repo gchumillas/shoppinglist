e770745e45b154e28fcaef56b22f3bb91d116393
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-11-29 13:03:59 +0100

    eslint: add arrow-parens rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
         "eqeqeq": "off",
         "react/prop-types": "off",
         "react/jsx-indent": [2, 2],
-        "react/jsx-indent-props": [2, 2]
+        "react/jsx-indent-props": [2, 2],
+        "arrow-parens": ["error", "as-needed"]
     }
 };
