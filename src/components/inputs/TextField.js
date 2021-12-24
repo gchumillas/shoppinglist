ddefe6018e5426b1364bb72a95a9910aa8bb4c46
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-12-24 08:39:01 +0100

    remove extra spaces

--- src/components/inputs/TextField.js
+++ src/components/inputs/TextField.js
@@ -2,16 +2,30 @@ import React from 'react'
 import { TextInput, StyleSheet, View, Text } from 'react-native'
 import { tw } from '~/src/libs/tailwind'
 
-const Component = ({ title = '', dense = false, autoFocus = false, ref, ...props }) => {
+const Component = ({ title = '', dense = false, autoFocus = false, value, onChangeText, ...props }) => {
   const inputRef = React.useRef(null)
+  const [internalValue, setInternalValue] = React.useState('')
+  const trimmedValue = React.useMemo(() => internalValue.trim(), [internalValue])
 
   React.useEffect(() => {
     autoFocus && inputRef.current && setTimeout(() => inputRef.current.focus(), 40)
   }, [inputRef.current])
 
+  React.useEffect(() => {
+    if (trimmedValue != value) {
+      setInternalValue(value)
+    }
+  }, [value])
+
+  React.useEffect(() => {
+    if (trimmedValue != value) {
+      onChangeText(trimmedValue)
+    }
+  }, [trimmedValue])
+
   return <View style={[styles.wrapper, dense && styles.dense]}>
     {!!title && <Text style={styles.title}>{title}</Text>}
-    <TextInput {...props} ref={inputRef} style={styles.input} />
+    <TextInput {...props} ref={inputRef} style={styles.input} value={internalValue} onChangeText={setInternalValue} />
   </View>
 }
 
