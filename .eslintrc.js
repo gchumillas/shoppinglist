eec31a262f90c2c44ae23715e047676f7387063a
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-12-24 18:33:04 +0100

    eslint: update rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,9 +24,17 @@ module.exports = {
     ],
     "rules": {
         "eqeqeq": "off",
+        "arrow-parens": ["error", "as-needed"],
         "react/prop-types": "off",
         "react/jsx-indent": [2, 2],
         "react/jsx-indent-props": [2, 2],
-        "arrow-parens": ["error", "as-needed"]
+        "react/jsx-boolean-value": ["error", "never"],
+        "react/jsx-closing-bracket-location": "error",
+        "react/jsx-curly-newline": "error",
+        "react/jsx-curly-spacing": "error",
+        "react/jsx-equals-spacing": ["error", "never"],
+        "react/jsx-no-constructed-context-values": "error",
+        "react/jsx-props-no-multi-spaces": "error",
+        "react/jsx-tag-spacing": "error"
     }
 };
