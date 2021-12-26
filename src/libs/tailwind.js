22cce18ac4336f6506e5882695be208e65a7e703
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-12-26 23:52:26 +0100

    add inline to-dos

--- src/libs/tailwind.js
+++ src/libs/tailwind.js
@@ -2,6 +2,7 @@ import { create } from 'tailwind-rn'
 import styles from '~/tailwind.styles.json'
 
 const { tailwind, getColor } = create(styles)
+// TODO: remove Palette (use getColor('primary') instead)
 const Palette = {
   PrimaryText: getColor('primary')
 }
