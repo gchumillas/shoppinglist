3d296d6c9282bc196bbcff0f5276cfbea922cbfc
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-12-26 23:53:09 +0100

    remove Palette const

--- src/libs/tailwind.js
+++ src/libs/tailwind.js
@@ -2,9 +2,4 @@ import { create } from 'tailwind-rn'
 import styles from '~/tailwind.styles.json'
 
 const { tailwind, getColor } = create(styles)
-// TODO: remove Palette (use getColor('primary') instead)
-const Palette = {
-  PrimaryText: getColor('primary')
-}
-
-export { tailwind as tw, getColor, Palette }
+export { tailwind as tw, getColor }
