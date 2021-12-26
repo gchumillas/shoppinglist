7300ed5736d0df82c1b9fd3c1e2e3643796ef455
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-12-26 23:07:22 +0100

    bugfix

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -3,5 +3,5 @@
  * @returns number[]
  */
 export const range = ({ from = 0, to }) => {
-  return Array.from(Array(to - from + 1).keys()).map(i => i + from)
+  return Array.from(Array(Math.max(0, to - from + 1)).keys()).map(i => i + from)
 }
