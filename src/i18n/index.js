d18a082a555f9ae3a964373015ac402bbdee61b9
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-12-28 20:13:05 +0100

    i18n: add default locale

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,5 +3,6 @@ import i18n from 'i18n-js'
 import translations from './translations'
 
 i18n.translations = translations
+i18n.defaultLocale = 'en'
 i18n.locale = Localization.locale
 i18n.fallbacks = true
