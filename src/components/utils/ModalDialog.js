80d47a03a33c5b0fdc964e7617ba2461e73c378b
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-12-28 11:10:07 +0100

    add width attr

--- src/components/utils/ModalDialog.js
+++ src/components/utils/ModalDialog.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { Modal, Pressable, StyleSheet, View } from 'react-native'
 import { tw } from '~/src/libs/tailwind'
 
-const Component = ({ visible = false, onRequestClose = undefined, children }) => {
+const Component = ({ visible = false, width = 'xs', onRequestClose = undefined, children }) => {
   return <Modal animationType="fade" transparent visible={visible} onRequestClose={onRequestClose}>
     <Pressable onPress={onRequestClose}>
       <View style={styles.wrapper}>
-        <View style={styles.box} onStartShouldSetResponder={_ => true}>
+        <View
+          onStartShouldSetResponder={_ => true}
+          style={{ ...styles.box, ...width != 'none' && tw(`w-full max-w-${width}`) }}
+        >
           {children}
         </View>
       </View>
@@ -16,7 +19,7 @@ const Component = ({ visible = false, onRequestClose = undefined, children }) =>
 
 const styles = StyleSheet.create({
   wrapper: tw('flex h-full justify-start items-center pt-10'),
-  box: tw('bg-white p-5 max-w-xs w-full border rounded-lg')
+  box: tw('bg-white p-5 border rounded-lg')
 })
 
 export default Component
