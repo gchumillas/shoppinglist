aad46705a293ea2fbf90d4f877281b1b11ac711d
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-12-29 21:21:43 +0100

    rename page

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { NativeRouter, Routes, Route } from 'react-router-native'
 import store from '~/src/store'
 import '~/src/i18n'
 import NewArticlePage from '~/src/pages/NewArticlePage'
-import EditArticle from '~/src/pages/EditArticle'
+import EditArticlePage from '~/src/pages/EditArticlePage'
 import DeleteAllArticles from '~/src/pages/DeleteAllArticles'
 import RecorderPage from '~/src/pages/RecorderPage'
 import SettingsPage from '~/src/pages/SettingsPage'
@@ -17,7 +17,7 @@ const Root = () => {
       <Routes>
         <Route path="/" element={<App />}>
           <Route path="/new-article" element={<NewArticlePage />} />
-          <Route path="/edit-article/:id" element={<EditArticle />} />
+          <Route path="/edit-article/:id" element={<EditArticlePage />} />
           <Route path="/delete-all-articles" element={<DeleteAllArticles />} />
           <Route path="/recorder" element={<RecorderPage />} />
           <Route path="/settings" element={<SettingsPage />} />
