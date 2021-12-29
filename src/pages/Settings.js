f292d79527a26b5e3dc13b2833852693d3c6b8e1
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-12-29 16:50:56 +0100

    use store hooks

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -5,14 +5,15 @@ import Checkbox from 'expo-checkbox'
 import { Picker } from '@react-native-picker/picker'
 import { tw } from '~/src/libs/tailwind'
 import { useTranslation } from '~/src/hooks/i18n'
+import { useLanguage, useDetectLanguage } from '~/src/hooks/store'
 import { Button } from '~/src/components/inputs'
 import { ModalDialog } from '~/src/components/utils'
 
 const Component = _ => {
   const t = useTranslation('pages.settings')
   const navigate = useNavigate()
-  const [systemLangSelected, setSystemLangSelected] = React.useState(false)
-  const [language, setLanguage] = React.useState('en')
+  const [detectLanguage, setDetectLanguage] = useDetectLanguage()
+  const [language, setLanguage] = useLanguage()
 
   const doSave = async _ => {
     navigate('/')
@@ -20,9 +21,13 @@ const Component = _ => {
 
   return <ModalDialog visible>
     <View>
-      <Checkbox value={systemLangSelected} onValueChange={setSystemLangSelected} />
-      <Text>Detect language</Text>
-      <Picker selectedValue={language} onValueChange={setLanguage}>
+      <View style={tw('flex flex-row items-center')}>
+        <Checkbox value={detectLanguage} onValueChange={setDetectLanguage} />
+        <Text style={tw('pl-2')}>Detect language</Text>
+      </View>
+      {/* TODO: (all) replace tw('...') by tw`...` */}
+      <Picker selectedValue={language} onValueChange={setLanguage} style={tw('bg-gray-300')}>
+        <Picker.Item label="No language selected" value="" />
         <Picker.Item label="English" value="en" />
         <Picker.Item label="Spanish" value="es" />
       </Picker>
