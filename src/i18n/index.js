0c2ce960733e30aa57cf83b6b396aa0bd24fa640
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-12-29 19:01:21 +0100

    rename folder

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import * as Localization from 'expo-localization'
-import resources from './translations'
+import resources from './resources'
 
 i18n
   .use(initReactI18next)
