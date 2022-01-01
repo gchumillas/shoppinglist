4e588299478b23b0e6e96658ebac8c30df0311f3
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-01-01 23:08:29 +0100

    refactor

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ import { NativeRouter, Routes, Route } from 'react-router-native'
 import { registerRootComponent } from 'expo'
 import { useFonts } from 'expo-font'
 import { StatusBar } from 'expo-status-bar'
+import * as Localization from 'expo-localization'
 import { RobotoSlab_500Medium } from '@expo-google-fonts/roboto-slab'
 import { loadStore } from '~/src/store'
 import { tw, getColor } from '~/src/libs/tailwind'
-import '~/src/i18n'
+import { useLanguage } from '~/src/hooks/store'
+import i18n from '~/src/i18n'
 import HomePage from '~/src/pages/HomePage'
 import NewArticlePage from '~/src/pages/NewArticlePage'
 import EditArticlePage from '~/src/pages/EditArticlePage'
@@ -16,7 +18,27 @@ import DeleteAllArticlesPage from '~/src/pages/DeleteAllArticlesPage'
 import RecorderPage from '~/src/pages/RecorderPage'
 import SettingsPage from '~/src/pages/SettingsPage'
 
-const Root = () => {
+const App = _ => {
+  const [language] = useLanguage()
+
+  React.useEffect(() => {
+    i18n.changeLanguage(language || Localization.locale)
+  }, [language])
+
+  return <NativeRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />}>
+        <Route path="/new-article" element={<NewArticlePage />} />
+        <Route path="/edit-article/:id" element={<EditArticlePage />} />
+        <Route path="/delete-all-articles" element={<DeleteAllArticlesPage />} />
+        <Route path="/recorder" element={<RecorderPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+      </Route>
+    </Routes>
+  </NativeRouter>
+}
+
+const AppLoader = _ => {
   const [fontsLoaded] = useFonts({ RobotoSlab_500Medium }) // eslint-disable-line camelcase
   const [value, setValue] = React.useState()
 
@@ -26,17 +48,7 @@ const Root = () => {
 
   return value && fontsLoaded
     ? <Provider store={value}>
-      <NativeRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />}>
-            <Route path="/new-article" element={<NewArticlePage />} />
-            <Route path="/edit-article/:id" element={<EditArticlePage />} />
-            <Route path="/delete-all-articles" element={<DeleteAllArticlesPage />} />
-            <Route path="/recorder" element={<RecorderPage />} />
-            <Route path="/settings" element={<SettingsPage />} />
-          </Route>
-        </Routes>
-      </NativeRouter>
+      <App />
     </Provider>
     : <View style={tw`flex h-full justify-center items-center`}>
       <ActivityIndicator size={55} color={getColor('gray-600')} />
@@ -44,4 +56,4 @@ const Root = () => {
     </View>
 }
 
-registerRootComponent(Root)
+registerRootComponent(AppLoader)
--- src/layouts/PageLayout.js
+++ src/layouts/PageLayout.js
@@ -1,21 +1,12 @@
 import React from 'react'
 import { View } from 'react-native'
-import * as Localization from 'expo-localization'
 import { StatusBar } from 'expo-status-bar'
 import { tw } from '~/src/libs/tailwind'
-import i18n from '~/src/i18n'
-import { useLanguage } from '~/src/hooks/store'
 import BgImage from '~/src/components/BgImage'
 
 const Component = ({ children }) => {
-  // TODO: move this to the index.js file
-  const [language] = useLanguage()
   const [bgWidth, setBgWidth] = React.useState(0)
   const [bgHeight, setBgHeight] = React.useState(0)
-
-  React.useEffect(() => {
-    i18n.changeLanguage(language || Localization.locale)
-  }, [language])
 
   const doLayout = e => {
     const { nativeEvent: { layout } } = e
