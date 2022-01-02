39ec8ad8e1350aa51ff48fd2e08475a888a9667a
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-01-02 11:35:44 +0100

    css update

--- src/components/inputs/Button.js
+++ src/components/inputs/Button.js
@@ -5,13 +5,13 @@ import { tw } from '~/src/libs/tailwind'
 
 const Component = ({ title, primary = false, disabled = false, ...props }) => {
   return <Pressable style={cn(styles, 'wrapper', { primary })} disabled={disabled} {...props}>
-    <Text style={cn(styles, { primaryText: primary, disabledText: disabled })}>{title}</Text>
+    <Text style={cn(styles, 'text', { primaryText: primary, disabledText: disabled })}>{title}</Text>
   </Pressable>
 }
 
 const styles = StyleSheet.create({
   wrapper: tw`py-1.5 px-3 rounded-md`,
-  text: { ...tw`uppercase font-bold` },
+  text: { ...tw`text-base text-primary`, fontFamily: 'RobotoSlab_500Medium' },
   primary: tw`bg-gray-500 text-primary-light`,
   primaryText: tw`text-primary-light`,
   disabledText: tw`text-gray-400`
