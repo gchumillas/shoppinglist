952ab380fe58fc3fa950e978322c3aa8c732b7aa
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-01-03 11:56:55 +0100

    css update

--- src/components/inputs/Button.js
+++ src/components/inputs/Button.js
@@ -4,17 +4,23 @@ import cn from 'react-native-classnames'
 import { tw } from '~/src/libs/tailwind'
 
 const Component = ({ title, primary = false, disabled = false, ...props }) => {
-  return <Pressable style={cn(styles, 'wrapper', { primary })} disabled={disabled} {...props}>
+  return <Pressable style={cn(styles, 'wrapper', { primary, disabled })} disabled={disabled} {...props}>
     <Text style={cn(styles, 'text', { primaryText: primary, disabledText: disabled })}>{title}</Text>
   </Pressable>
 }
 
 const styles = StyleSheet.create({
-  wrapper: tw`py-1.5 px-3 rounded-md`,
-  text: { ...tw`text-base text-primary`, fontFamily: 'RobotoSlab_500Medium' },
-  primary: tw`bg-gray-500 text-primary-light`,
-  primaryText: tw`text-primary-light`,
-  disabledText: tw`text-gray-400`
+  wrapper: tw`py-1 pb-0.5 px-0.5 border-b-4 border-transparent`,
+  primary: tw`border-primary`,
+  disabled: tw`border-gray-300`,
+  text: {
+    ...tw`text-base text-primary`,
+    fontFamily: 'RobotoSlab_700Bold'
+  },
+  primaryText: {
+    fontFamily: 'RobotoSlab_900Black'
+  },
+  disabledText: tw`text-gray-300`
 })
 
 export default Component
