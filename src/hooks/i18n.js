b4844e6eace284ab03fcab931f4586f07a11d472
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-01-07 11:29:21 +0100

    remove unused library

--- src/hooks/i18n.js
+++
@@ -1,6 +0,0 @@
-// import i18n from 'i18n-js'
-import i18n from '~/src/i18n'
-
-export const useTranslation = (section = '') => {
-  return key => i18n.t([section, key].filter(x => !!x).join('.'))
-}
